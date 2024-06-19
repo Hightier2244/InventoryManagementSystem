fc765b4890cd555af078a50fac088c44eb4a988f
Hightier2244 <116667719+Hightier2244@users.noreply.github.com>
2024-06-19 19:04:51 +0200

    Changed the averagePrice function to take the amount in account

--- sql/function.sql
+++ sql/function.sql
@@ -3,6 +3,7 @@ CREATE OR ALTER FUNCTION SRaichle_CalculateAveragePrice()
 AS
 BEGIN
     DECLARE @AvgPrice DECIMAL(10, 2)
-    SELECT @AvgPrice = AVG(Price) FROM db_owner.SRaichle_Products
+    SELECT @AvgPrice = SUM(Price * Quantity) / SUM(Quantity)
+    FROM db_owner.SRaichle_Products
     RETURN @AvgPrice
 END
